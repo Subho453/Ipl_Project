d52e54282eff4c52a5c98b75a4101eb409026415
Subhojeet Saha <subhojeet.saha@mountblue.io>
2019-09-06 17:16:08 +0530

    merge matchesperyear

---
+++ index.js
@@ -0,0 +1,34 @@
+const csvFilePath='matches.csv'
+const writeJsonFile = require('write-json-file');
+const csv=require('csvtojson')
+csv()
+.fromFile(csvFilePath)
+.then((jsonObj)=>{
+    // console.log(jsonObj);
+    (async () => {
+    await writeJsonFile('matches.json',jsonObj);
+    })();
+
+})
+const matches=require('./matches.json');
+function matchesPerYear(){
+   var matchesPerYear= matches.reduce((acc,year) => {
+        if(acc[year.season]){
+            acc[year.season]++;
+        }
+        else{
+            acc[year.season]=1;
+        }
+        return acc;
+    },{});  
+     matchesPerYears={"Year":Object.keys(matchesPerYear),"Matches":Object.values(matchesPerYear)};
+    return matchesPerYears;
+    
+   
+}
+matchesPerYear();
+let data={matchesPerYears};
+     (async () => {
+        await writeJsonFile('public/data.json',data);
+
+     })();
