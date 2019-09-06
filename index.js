a2817e69a891722f07c746e8950b9ab0397e693c
Subhojeet Saha <subhojeet.saha@mountblue.io>
2019-09-06 17:17:59 +0530

    merge teamwonperyear

--- index.js
+++ index.js
@@ -23,11 +23,37 @@ function matchesPerYear(){
     },{});  
      matchesPerYears={"Year":Object.keys(matchesPerYear),"Matches":Object.values(matchesPerYear)};
     return matchesPerYears;
-    
-   
 }
+function teamWonPerYear(){
+    let teamWon=matches.filter(match => match.winner!='')
+                       .reduce((acc,team)=>{
+        if(acc[team.winner]){
+            eachyear={};
+            matches.map(match=>eachyear[match.season]=0);
+           let matchcount=matches.filter(teams =>teams.winner===team.winner)
+                                 .reduce((acc,year)=>{
+                if(acc[year.season]){
+                    acc[year.season]++;
+                }
+                else{
+                    acc[year.season]=1;
+                }
+                return acc;
+            },eachyear);
+            acc[team.winner]=matchcount;
+        }
+        else{
+            acc[team.winner]={};
+        }
+        return acc;
+    },{});
+    console.log(teamWon);
+    teamWons={'Teams':Object.keys(teamWon),'Years':Object.values(teamWon)};
+    return teamWons;
+}
+teamWonPerYear();
 matchesPerYear();
-let data={matchesPerYears};
+let data={matchesPerYears,teamWons};
      (async () => {
         await writeJsonFile('public/data.json',data);
 
