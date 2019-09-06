a8fc1c3f37fada40af4ea877ba7e661285943779
Subhojeet Saha <subhojeet.saha@mountblue.io>
2019-09-06 09:53:07 +0530

    initial

--- index.js
+++ index.js
@@ -3,3 +3,15 @@
 2. Calls functions implemented ipl module
 3. Stores output at `public/data.json`
 */
+const csvFilePath='matches.csv'
+const writeJsonFile = require('write-json-file');
+const csv=require('csvtojson')
+csv()
+.fromFile(csvFilePath)
+.then((jsonObj)=>{
+    // console.log(jsonObj);
+    (async () => {
+    await writeJsonFile('matches.json',jsonObj);
+    })();
+
+})
